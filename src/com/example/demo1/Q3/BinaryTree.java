0cc9d7a4db223f548ff223e3362231c68b0a91a0
Hammaad Rizwan <65250595+hammaadrizwan@users.noreply.github.com>
2023-03-30 17:35:47 +0530

    All methods work, stable version

--- src/com/example/demo1/Q3/BinaryTree.java
+++ src/com/example/demo1/Q3/BinaryTree.java
@@ -1,22 +1,29 @@
 package com.example.demo1.Q3;
 
 public class BinaryTree {
-    private Node root;//initially ythe tree has the root node
+    private Node root;//initially the tree has the root node
 
-    public Node find(String data){
-        Node node = root;
-        while (node != null){//binary search is being imlpemented, Efficeint compared to linear search with Linked List.
-            if (node.data.compareTo(data)==0){// when both of the strings are equal that mean value found
-                return node;
-            }
-            if (node.data.compareTo(data)<0){ //if the data stored in the tree is less than the data searched then move to the right subtree
-                node = node.rightChild;
-            }
-            else{
-                node = node.leftChild; // else continue looking on the left side
-            }
+    // recursive function to search the value
+    private Node finding(String expected, Node node) {
+        if (node == null) {//binary search is being imlpemented, Efficeint compared to linear search with Linked List.
+            return null;// if not found returns null
+        } else if (node.data.equalsIgnoreCase(expected)) {// when both of the strings are equal that mean value found
+            return node;
+        } else if (node.data.compareToIgnoreCase(expected) < 0) {//if the data stored in the tree is less than the data searched then move to the right subtree
+            return finding(expected, node.rightChild);
+        } else {
+            return finding(expected, node.leftChild);// else continue looking on the left side
         }
-        return null; // if not found returns null
+    }
+
+    // search for a value starting at the root - this metohd start searching from root
+    public String find(String expected) {
+        Node node = finding(expected, root);//this gets the value from the recursive function call
+        if (node == null) {
+            return null;
+        } else {
+            return node.data;
+        }
     }
 
     public void insert(String data){
@@ -36,104 +43,6 @@ public class BinaryTree {
         return node;//returns the node to the recent function call.
     }
 
-    public void display(){
-        displaying(root);//same as inserting but here we use recursion to display the tree
-    }
-    private void displaying(Node node){
-        if (node !=null){//until all the elements has been displayed first the all the left side nodes are being displayed then the rightside nodes. This shows how the final sorted function works using Search trees.
-            displaying(node.leftChild);
-            System.out.println(node.data+" ");
-            displaying(node.rightChild);
-        }
     }
 
 
-
-
-
-
-
-
-
-
-
-//    private Node root;
-//
-//    // set the whole tree to null
-//    public BinaryTree() {
-//        root = null;
-//    }
-//
-//    // perform inorder traversal on the tree
-//    private int inorderPrint(Node node) {
-//        if (node != null) {
-//            inorderPrint(node.left);
-//            System.out.print(node.data + " ");
-//            inorderPrint(node.right);
-//        }
-//    }
-//
-//    // print the entire tree
-//    public void print() {
-//        int count = inorderPrint(root);
-//        System.out.println();
-//        System.out.println(count);
-//    }
-//
-//    // recursive function to insert at a particular node
-//    private Node insertAt(String value, Node node) {
-//        if (node == null) {
-//            return new Node(value);
-//        }
-//
-//        if (node.data.compareTo(value) < 0) {
-//            // right
-//            node.right = insertAt(value, node.right);
-//        } else {
-//            // left
-//            node.left = insertAt(value, node.left);
-//        }
-//
-//        return node;
-//    }
-//
-//    // insert a number calls recursive function to insert at the root
-//    public void insert(String value) {
-//        root = insertAt(value, root);
-//    }
-//
-//    // perform the recursive search at a given node
-//    private Node searchAt(String target, Node node) {
-//        if (node == null) {
-//            return null;
-//        } else if (node.data.equals(target)) {
-//            return node;
-//        } else if (node.data.compareTo(target) < 0) {
-//            return searchAt(target, node.right);
-//        } else {
-//            return searchAt(target, node.left);
-//        }
-//    }
-//
-//    // search for a value and return it, or null if not found
-//    public String search(String target) {
-//        Node node = searchAt(target, root);
-//        if (node == null) {
-//            return null;
-//        } else {
-//            return node.data;
-//        }
-//    }
-//
-//    // find the smallest node value in a subtree
-//    private Node min(Node node) {
-//        if (node == null) {
-//            return null;
-//        } else if (node.left == null) {
-//            return node;
-//        } else {
-//            return min(node.left);
-//        }
-//    }
-
-}
--- src/com/example/demo1/Q3/Main.java
+++ src/com/example/demo1/Q3/Main.java
@@ -1,32 +1,201 @@
 package com.example.demo1.Q3;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         BinaryTree binaryTree;
-        String data;
         binaryTree = readFromFile("src/com/example/demo1/Q3/data.txt");// reads the words from the text file and populates the tree accordingly
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter the word to find");
-        data = input.next();
-        while(!data.equals("q")){
-            if(!spellChecker(binaryTree,data).equals("Not Found")){;//if the word is found then display else find the word that was meant to be searched.
-                System.out.println(spellChecker(binaryTree,data));
-            }else {
-                ArrayList<String> possibleWords = removeCharacter(binaryTree,data);
-                for (String word:possibleWords) {
-                    System.out.println(word);
+
+        while(true){
+            System.out.print("Enter the word to find: ");//asks the user to enter teh word to find
+            String data = input.next();
+
+            if (data.equals("q")){break;} //exit condition
+
+            ArrayList<String> listContainingWords = spellChecker(binaryTree,data);//stores the list of words returned from the spellchecker
+
+            if (listContainingWords.get(0).equals("suggestions>>")){ // the word is incorrect, the list of words will start with the string"suggestions>>", to signal its incorrect
+                if(listContainingWords.size()>1){//if that list has more than one element it means there are suggesstions,
+                    System.out.print("Did you mean?  ");
+                    for (String possibleWord:spellChecker(binaryTree,data)) {
+                        if (possibleWord.equals("suggestions>>")){//prints all the words after suggesttions
+                            continue;
+                        }
+                        System.out.print(possibleWord.toLowerCase()+" ");
+                    }
+                    System.out.println();
+                }else{//if not there are no suggesstions
+                    System.out.println("Word is incorrect,no suggestions found");
                 }
+            }else {//if the list isnot a suggesstions list then print the word, since it is corect
+                String wordFound = listContainingWords.get(0);
+                System.out.println(wordFound);
             }
-            data = input.next();
+            System.out.println();
+
         }
     }
 
+    public static ArrayList<String> spellChecker(BinaryTree binaryTree,String value){
+        ArrayList<String> listOfWords = new ArrayList<>();//creates a listOfWords array
+        if(binaryTree.find(value)==null){// if word is not found
+            if(findPossibleWords(binaryTree, value).size()>0){//finds all the possible words
+                listOfWords = findPossibleWords(binaryTree, value);//adds the possible words to the list ofwords
+            }//returns the list of all the possiblewords
+            return listOfWords;
+        }
+        else {
+            listOfWords.add(binaryTree.find(value));//adds the word to the list of words. as requried
+            return (listOfWords);
+        }
+    }
+
+    public static ArrayList<String> findPossibleWords(BinaryTree binaryTree,String data){
+        ArrayList<String> allPossibleWords = new ArrayList<>();// this stores all the possible words that could have been meant
+        ArrayList<String> removeCharacterPossibleWords =removeCharacter(binaryTree,data);//loads the list generated from each method
+        ArrayList<String> insertCharacterPossibleWords =insertCharacter(binaryTree,data);
+        ArrayList<String> swapCharacterPossibleWords =swapCharacter(binaryTree,data);
+        ArrayList<String> replaceCharacterPossibleWords =replaceCharacter(binaryTree,data);
+
+        allPossibleWords.add("suggestions>>");//since allof thesse are suggesstions the list returned will have the first element with the tag "suggesstions"
+        allPossibleWords.addAll(removeCharacterPossibleWords);
+        allPossibleWords.addAll(insertCharacterPossibleWords);
+        allPossibleWords.addAll(swapCharacterPossibleWords);
+        allPossibleWords.addAll(replaceCharacterPossibleWords);//combines all the words from the methods and removes the duplicates
+
+        ArrayList<String> allPossibleWordsRemovedDuplicates = new ArrayList<>();
+        for (String word:allPossibleWords) {
+            if(!allPossibleWordsRemovedDuplicates.contains(word)){//if the new list contains the word, thene dont insert it
+                allPossibleWordsRemovedDuplicates.add(word);
+            }
+        }
+        return allPossibleWordsRemovedDuplicates; //returns all the possibilities
+    }
+
+
+    //REMOVE CHARACTER METHOD
+    public static ArrayList<String> removeCharacter(BinaryTree binaryTree, String data){//takes the tree and data as parameters like other functions
+        ArrayList<String> possibleWords = new ArrayList<>();
+        for(int i = 0;i<data.length();i++){//for each letter in the word
+            char[] letters = data.toCharArray();
+            letters[i]=' ';//converts that letter to a blank
+            String removedLetter = new String(letters);//then creates a new array which is a combination of the list of characters
+            String newWord = removedLetter.replace(" ","");//using the replace command to replace the space with no space,and stores this as a new word
+            if (binaryTree.find(newWord)!=(null)) {//we find if this new word is in the binary tree
+                possibleWords.add(newWord);//if its availalble we add to the list of possible words
+            }
+        }
+        return possibleWords;
+    }
+
+    //INSERT CHARACTER METHOD
+    private static char[] addLetter(int position, char[] letters, char letterToAdd) {//this function is only accessible to the insert character method,it takes the values from that function call
+        char[] newArray = new char [letters.length+1];// new array with an additional slot,since we have to store this new letter.
+
+        for (int outerLoop = 0, innerLoop = 0; outerLoop < letters.length; outerLoop++, innerLoop++) {//we have to copy the values from the prevous array and then if the positon is hit, then we add the character we need to the array,and continue copying the rest of the characters
+            if (outerLoop == position && outerLoop!=letters.length) {
+                newArray[innerLoop++] = letterToAdd;//inserting the new value to the correct slot
+            }
+            newArray[innerLoop] = letters[outerLoop];//copying the value from the original list
+        }
+        if (position == letters.length) {//if the index that we need to insert the value is in the last position
+            newArray[letters.length] = letterToAdd;//we add it to the correct positon in the new array, this condition is not being met by the for loop therefore its used here.
+        }
+
+        return newArray;
+    }
+
+    public static ArrayList<String> insertCharacter(BinaryTree binaryTree, String data){
+        ArrayList<String> possibleWords = new ArrayList<>();
+
+        ArrayList<Character> characters = new ArrayList<>();//creates a list of characters from the aplhpabet
+        for(char character = 'a'; character <= 'z'; character++){
+            characters.add(character);
+        }
+
+        char[] exsistingLetters=data.toCharArray();//converts the word to a list of letters
+        for (int index = 0; index <= exsistingLetters.length; index++) {// for each index in the word
+            for (Character character: characters) {//test with all the characters
+                char newLetter = character;
+                char[] addedNewLetter = addLetter(index,exsistingLetters,newLetter);//calls the addLetter fucntion to add the character to the relavent index position
+                String newWord= new String(addedNewLetter);//converts the new list to a new word
+                if (binaryTree.find(newWord)!=(null)) {//if its found in the binary tree then this  could be a possible word
+                    possibleWords.add(newWord);
+                }
+            }
+        }
+        return possibleWords;
+    }
+
+    //SWAP CHARACTER METHOD
+    public static ArrayList<String> swapCharacter(BinaryTree binaryTree, String data){
+        ArrayList<String> possibleWords = new ArrayList<>();
+        char[] letters = data.toCharArray();
+//        StringBuilder sb = new StringBuilder(data);
+//        for (int i = 0; i < sb.length() - 1; i++) {
+//            char temp = sb.charAt(i);
+//            sb.setCharAt(i, sb.charAt(i + 1));
+//            sb.setCharAt(i + 1, temp);
+//            i++; // move to the next even/odd index
+//            if (binaryTree.find(sb.toString())!=(null)) {//if its found in the binary tree then this  could be a possible word
+//                possibleWords.add(sb.toString());
+//            }
+//        }
+
+        int i=0;
+        int j=0;
+        int swaps=0;
+        while (swaps<letters.length){
+            letters = data.toCharArray();
+            if (j==letters.length-1){
+                char temp = letters[j];
+                letters[j]=letters[letters.length-1];
+                letters[letters.length-1]=temp;
+            }else{
+                char temp = letters[j];
+                letters[j]=letters[j+1];
+                letters[j+1]=temp;
+            }
+            swaps++;
+            i++;
+            j=i;
+            String newWord= new String(letters);
+            if (binaryTree.find(newWord)!=(null)) {//if its found in the binary tree then this  could be a possible word
+                possibleWords.add(newWord);
+                return possibleWords;
+            }
+        }
+        return possibleWords;
+    }
+
+    //REPLACE CHARACTER METHOD
+    public static ArrayList<String> replaceCharacter(BinaryTree binaryTree, String data) {
+        ArrayList<String> possibleWords = new ArrayList<>();
+        ArrayList<Character> characters = new ArrayList<>();
+        for (char character = 'a'; character <= 'z'; character++) {//same as inserting a character to the word, we generate all the possible characters
+            characters.add(character);
+        }
+        char[] letters = data.toCharArray();
+        for (int i = 0; i < letters.length; i++) {// for each index in the word,
+            for (int j = 0; j < characters.size(); j++) {//generate all the possible characters that could have been entered
+                letters[i] = characters.get(j);//sets the new chracter to the letters at that index position
+                String newWord = new String(letters);
+                if (binaryTree.find(newWord) != (null)) {//checks if that is a word, does this untill all the characters are being entered to all the positions of the word
+                    possibleWords.add(newWord);
+                }
+            }
+        }
+        return possibleWords;
+    }
+
+
+    //POPULATE THE BINARY TRESS FROM FILE
     public static BinaryTree readFromFile(String filename) throws FileNotFoundException {
         BinaryTree binaryTree = new BinaryTree();
         File file = new File(filename);
@@ -39,26 +208,5 @@ public class Main {
         return binaryTree;
     }
 
-    public static String spellChecker(BinaryTree binaryTree,String value){
-        if(binaryTree.find(value)!=null){
-            return value;
-        }
-        return "Not Found";
-    }
 
-    public static ArrayList<String> removeCharacter(BinaryTree binaryTree, String data){
-        ArrayList<String> possibleWords = new ArrayList<>();
-        for(int i = 0;i<data.length();i++){
-            String newWord = data;
-            String letter= String.valueOf(data.charAt(i));
-            newWord = newWord.replace(letter,"");
-            if (binaryTree.find(newWord)==(null)){
-                continue;
-            }else {
-                possibleWords.add(newWord);
-            }
-
-        }
-        return possibleWords;
-    }
 }
